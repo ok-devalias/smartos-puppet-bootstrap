b00f8cf44be72f9173b61d36da5e4138b4661bd6
Josh Moore <salsasombrero@hotmail.com>
2014-03-18 16:27:05 -0400

    Easier to read variable

--- pupmodule-inst.sh
+++ pupmodule-inst.sh
@@ -13,6 +13,8 @@ SunOS)
 	PUPPET_ROOT="/etc/puppet"
 ;;	
 esac
+MODULE_DIR="$PUPPET_ROOT/modules"
+
 echo "Checking for needed puppet modules."
 if [ ! "$(puppet module list | grep $NGINX)" ]; then
 	echo "Installing module: $NGINX"
@@ -24,25 +26,25 @@ fi
 
 if [ "$OSBASE" == "SunOS" ]; then
 	echo "Checking $NGINX for SmartOS fixes."
-	if [ ! "$(grep sunos "$PUPPET_ROOT"/modules/nginx/manifests/params.pp)" ]; then
+	if [ ! "$(grep sunos "$MODULE_DIR"/nginx/manifests/params.pp)" ]; then
 		echo "Applying params.pp \$::kernel compatibility fix."
 		sed -i '/$::kernel ?/ {N; s/\(?i-mx:linux\)/\1\|sunos\)/}' "$PUPPET_ROOT/modules/nginx/manifests/params.pp"
 	else
 		echo "params.pp \$::kernel compatibility fix detected."
 	fi
-	if [ ! "$(grep solaris "$PUPPET_ROOT"/modules/nginx/manifests/params.pp)" ]; then
+	if [ ! "$(grep solaris "$MODULE_DIR"/nginx/manifests/params.pp)" ]; then
 		echo "Applying params.pp \$::osfamily compatibility fix."
 		sed -i '/$::osfamily ?/ {N; s/\(?i-mx:\)/\1solaris\|/}' "$PUPPET_ROOT/modules/nginx/manifests/params.pp"
 	else
 		echo "params.pp \$::osfamily compatibility fix detected."	
 	fi
-	if [ ! "$(grep smartos "$PUPPET_ROOT"/modules/nginx/manifests/params.pp)" ]; then
+	if [ ! "$(grep smartos "$MODULE_DIR"/nginx/manifests/params.pp)" ]; then
 		echo "Applying params.pp \$::operatingsystem compatibility fix."
 		sed -i 's/\(?i-mx:[a-z|]*|oraclelinux\)/\1|smartos/' "$PUPPET_ROOT/modules/nginx/manifests/params.pp"
 	else
 		echo "params.pp \$::operatingsystem compatibility fix detected."
 	fi
-	if [ ! "$(grep solaris "$PUPPET_ROOT"/modules/nginx/manifests/package.pp)" ]; then
+	if [ ! "$(grep solaris "$MODULE_DIR"/nginx/manifests/package.pp)" ]; then
 		echo "Applying package.pp \$::osfamily compatibility fix."
 		sed -i "/\$::osfamily ?/ {N; s/\('redhat'\)/\1, 'solaris'/ }" "$PUPPET_ROOT/modules/nginx/manifests/package.pp"
 	else
@@ -61,7 +63,7 @@ fi
 
 if [ "$OSBASE" == "SunOS" ]; then
 	echo "Checking $GIT for SmartOS fixes."
-	if [ ! "$(grep SmartOS "$PUPPET_ROOT"/modules/git/manifests/params.pp)" ]; then
+	if [ ! "$(grep SmartOS "$MODULE_DIR"/git/manifests/params.pp)" ]; then
 		echo "Applying params.pp \$::operatingsystem compatibility fix."
 		sed -i 's/$bin =/$bin = $::operatingsystem ? {\
 		\(SmartOS|Solaris\) => "\/opt\/local\/bin\/git"\
