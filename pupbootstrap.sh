da44b754a34f019ce457d0b2fb4f15de8d76f00f
Josh Moore <salsasombrero@hotmail.com>
2014-03-17 21:14:22 -0400

    added -y flag for apt-get

--- pupbootstrap.sh
+++ pupbootstrap.sh
@@ -153,7 +153,7 @@ debian|ubuntu)
 		`apt-get -q update >> aptget.log 2>&1`
 		echo "Done."
 		echo "Installing puppet and dependencies..."
-		`apt-get -q install puppet`
+		`apt-get -qy install puppet`
 		`rm -f /tmp/$PKG`
 		echo "Done."
 	else
