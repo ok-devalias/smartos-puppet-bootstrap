e0341651c6bf813ac692eb417287e0c30ef54bde
Josh Moore <salsasombrero@hotmail.com>
2014-03-18 11:29:11 -0400

    Add config checks, sed syntax update

--- pupmodule-inst.sh
+++ pupmodule-inst.sh
@@ -4,7 +4,8 @@
 OSBASE=$(uname)
 NGINX='jfryman-nginx'
 GIT='jproyo-git'
-case $OSBASE in
+VCSREPO='puppetlabs-vcsrepo'
+case "$OSBASE" in
 	SunOS*)
 		PUPPET_ROOT='/opt/local/etc/puppet'
 	;;
@@ -12,21 +13,67 @@ case $OSBASE in
 		PUPPET_ROOT='/etc/puppet'
 	;;	
 esac
-
-if [ ! -d "$PUPPET_ROOT"/modules/nginx ]; then
-	puppet module install $NGINX > /dev/null 2&>1
-	sed '/$::kernel ?/ {N; s/\(?i-mx:linux\)/\1\|sunos\)/}' < "$PUPPET_ROOT"/modules/nginx/manifests/params.pp > "$PUPPET_ROOT"/modules/nginx/manifests/params.pp
-	sed '/$::osfamily ?/ {N; s/\(?i-mx:\)/\1solaris\|/}' < "$PUPPET_ROOT"/modules/nginx/manifests/params.pp > "$PUPPET_ROOT"/modules/nginx/manifests/params.pp
-	sed 's/\(?i-mx:[a-z|]*|oraclelinux\)/\1|smartos/' < "$PUPPET_ROOT"/modules/nginx/manifests/params.pp > "$PUPPET_ROOT"/modules/nginx/manifests/params.pp
-	
+echo "Checking for needed puppet modules."
+if [ ! $(puppet module list | grep "$NGINX") ]; then
+	echo "Installing module: $NGINX"
+	puppet module install "$NGINX" > /dev/null 2&>1
+	echo "Done."	
+else
+	echo "Module $NGINX detected."	
 fi
-if [ ! -d "$PUPPET_ROOT"/modules/git ]; then
-	puppet module install $GIT > /dev/null 2&>1
+
+if [ "$OSBASE" EQ 'SunOS*' ]; then
+	echo "Checking $NGINX for SmartOS fixes."
+	if [ ! $(grep sunos "$PUPPET_ROOT"/modules/nginx/manifests/params.pp) ]; then			
+		sed '/$::kernel ?/ {N; s/\(?i-mx:linux\)/\1\|sunos\)/}' "$PUPPET_ROOT"/modules/nginx/manifests/params.pp > /tmp/tmp-params.pp
+		mv /tmp/tmp-params.pp  "$PUPPET_ROOT"/modules/nginx/manifests/params.pp
+		rm -f /tmp/tmp-params.pp > /dev/null 2&>1
+	fi
+	if [ ! $(grep solaris "$PUPPET_ROOT"/modules/nginx/manifests/params.pp) ]; then
+		sed '/$::osfamily ?/ {N; s/\(?i-mx:\)/\1solaris\|/}' "$PUPPET_ROOT"/modules/nginx/manifests/params.pp > /tmp/tmp-params.pp
+		mv /tmp/tmp-params.pp  "$PUPPET_ROOT"/modules/nginx/manifests/params.pp
+		rm -f /tmp/tmp-params.pp > /dev/null 2&>1
+	fi
+	if [ ! $(grep smartos "$PUPPET_ROOT"/modules/nginx/manifests/params.pp) ]; then
+		sed 's/\(?i-mx:[a-z|]*|oraclelinux\)/\1|smartos/' "$PUPPET_ROOT"/modules/nginx/manifests/params.pp > /tmp/tmp-params.pp
+		mv /tmp/tmp-params.pp  "$PUPPET_ROOT"/modules/nginx/manifests/params.pp
+		rm -f /tmp/tmp-params.pp > /dev/null 2&>1
+	fi
+	if [ ! $(grep solaris "$PUPPET_ROOT"/modules/nginx/manifests/package.pp) ]; then
+		sed '$::osfamily {N; s/\(\'redhat\'\)/\1, \'solaris\'/}' "$PUPPET_ROOT"/modules/nginx/manifests/package.pp > /tmp/tmp-package.pp
+		mv /tmp/tmp-package.pp "$PUPPET_ROOT"/modules/nginx/manifests/package.pp
+		rm -f /tmp/tmp-package.pp > /dev/null 2&>1
+	fi
+	echo "Done."		
+	fi
+fi
+
+if [ ! $(puppet module list | grep "$GIT") ]; then
+	echo "Installing module: $GIT"
+	puppet module install "$GIT" > /dev/null 2&>1
+	echo "Done."	
+fi
+
+if [ "$OSBASE" EQ 'SunOS*' ]; then
 	GIT_FIX="$bin = $::operatingsystem ? {
-    /(SmartOS|Solaris)/ => '/opt/local/bin/git'
-    default             => '/usr/bin/git'
+	/(SmartOS|Solaris)/ => '/opt/local/bin/git'
+	default             => '/usr/bin/git'
   }"
-  GIT_FIX_SAFE=$(printf  '%s\n' "$GIT_FIX"  | sed 's/[\&/]/\\&/g')
-  sed 's/$bin =/${GIT_FIX_SAFE}/' "$PUPPET_ROOT"/modules/git/manifests/params.pp  > "$PUPPET_ROOT"/modules/git/manifests/params.pp
+	GIT_FIX_SAFE=$(printf  '%s\n' "$GIT_FIX"  | sed 's/[\&/]/\\&/g')
+	if [ ! $(grep SmartOS "$PUPPET_ROOT"/modules/git/manifests/params.pp) ]; then
+		sed 's/$bin =/${GIT_FIX_SAFE}/' "$PUPPET_ROOT"/modules/git/manifests/params.pp  > /tmp/tmp-params.pp
+		mv /tmp/tmp-params.pp "$PUPPET_ROOT"/modules/git/manifests/params.pp
+		echo "Cleaning temp files..."
+		rm -f /tmp/tmp-params.pp > /dev/null 2&>1
+		echo "Done."
+	fi
 fi
 
+if [ ! $(puppet module list | grep "$VCSREPO") ]; then
+	echo "Installing module: $VCSREPO"
+	puppet module install "$VCSREPO" > /dev/null 2&>1
+	echo "Done."	
+fi
+echo 
+echo "All modules installed."
+echo
