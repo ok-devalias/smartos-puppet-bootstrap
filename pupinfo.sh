59f714f83f6a07cd1be5e28383d6095cf9532660
Josh Moore <salsasombrero@hotmail.com>
2014-03-19 01:17:05 -0400

    use ipadm on SmartOS instead of ip

--- pupinfo.sh
+++ pupinfo.sh
@@ -1,15 +1,18 @@
 # displays info for file locations and NGINX connection address
+DEST=$(grep nameserver /etc/resolv.conf -m 1| cut -d' ' -f2)
 OSBASE=$(uname)
 case $OSBASE in
 SunOS)
 	PUPPET_ROOT="/opt/local/etc/puppet"
+	IP=$(ipadm show-addr | grep "from-gz" | awk '{ print $NF }' | awk -F'/' '{ print $1 }')
 ;;
 *)
 	PUPPET_ROOT="/etc/puppet"
+	IP=$(ip route get "$DEST" | awk 'NR==1 {print $NF}')
 ;;	
 esac
-DEST=$(grep nameserver /etc/resolv.conf -m 1| cut -d' ' -f2)
-IP=$(ip route get "$DEST" | awk 'NR==1 {print $NF}')
+
+
 echo "--------------------------------------------"
 echo
 echo
