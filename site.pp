d689c3d2d4970968116dad2ede45b1cd174b43dd
Josh Moore <salsasombrero@hotmail.com>
2014-03-18 23:46:03 -0400

    Added cron to manifest

--- site.pp
+++ site.pp
@@ -1,11 +1,16 @@
 # /etc/puppet/manifests/site.pp
 node default {
   include nginx
-  include git
+  include git  
 
    $rootpath = $::operatingsystem ? {
      SmartOS => '/opt/local/share/www',
 	 default   => '/var/www/html',
+   }
+   
+   $puppetpath = $::operatingsystem ? {
+     SmartOS => '/opt/local/etc/puppet',
+	 default   => '/etc/puppet',
    }
    
   nginx::resource::vhost { 'puppetlabs_exercise':
@@ -19,4 +24,12 @@ node default {
 	provider	=> git,
     source    	=> 'https://github.com/puppetlabs/exercise-webpage.git',
   }
+  
+  cron::hourly{
+	  'pupapply.sh':
+		minute      		=> '11',
+		user        			=> 'root',
+		command    	=> "$puppetpath/pupapply.sh",
+		environment => "MAILTO=root\nPATH='/usr/bin:/bin'";
+	}
 }
