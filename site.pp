69787a08d5186c2ceff14f5266d136314bf3bbaa
Josh Moore <salsasombrero@hotmail.com>
2014-03-20 00:11:23 -0400

    set manage_repo = false

--- site.pp
+++ site.pp
@@ -1,17 +1,20 @@
 # /etc/puppet/manifests/site.pp
 node default {
-  include nginx
   include git
 
-   $rootpath = $::operatingsystem ? {
-     SmartOS => '/opt/local/share/www',
-	 default   => '/var/www/html',
-   }
+  $rootpath = $::operatingsystem ? {
+    SmartOS => '/opt/local/share/www',
+	default   => '/var/www/html',
+  }
    
    $puppetpath = $::operatingsystem ? {
      SmartOS => '/opt/local/etc/puppet',
 	 default   => '/etc/puppet',
    }
+   
+   class { 'nginx':
+    manage_repo => false,
+  }
       
   nginx::resource::vhost { 'puppetlabs_exercise':
     ensure    		=> present,
@@ -25,14 +28,7 @@ node default {
     source    	=> 'https://github.com/puppetlabs/exercise-webpage.git',
   }
   
-  vcsrepo { "$puppetpath/module/nginx":
-    ensure		=> present,	
-	provider	=> git,
-    source    	=> 'https://github.com/jfryman/puppet-nginx.git',
-	before		=> Nginx::resource::vhost['puppetlabs_exercise'],
-  }
-  
-  cron::hourly { 'pupapply.sh':
+   cron::hourly { 'pupapply.sh':
 		minute      		=> '11',
 		user        			=> 'root',
 		command    	=> "$puppetpath/pupapply.sh",
